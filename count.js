e70c04792c51454d126c5c6e28f59e9c852515e4
JM <j.maxwell.alan@gmail.com>
2021-10-30 01:18:16 -0400

    counting sort confirmd functional without array

--- count.js
+++ count.js
@@ -1,50 +1,31 @@
-const unsorted = [101, 4, 1, 207, 7, 5, 2, 6, 4, 7];
-const countSort = (arr, map, arrMax) => {
+const countSort = (arr) => {
   const counter = {};
   for (let i = 0; i < arr.length; i++) {
-    if (counter[arr[i]]) counter[arr[i]]++;
-    else counter[arr[i]] = 1;
+    const currentEl = arr[i];
+    if (counter[currentEl]) counter[currentEl]++;
+    else counter[currentEl] = 1;
   }
   let prevKey;
-  for (const key in counter) {
-    if (prevKey) counter[key] += counter[prevKey];
-    prevKey = key;
+  const counterKeys = Object.keys(counter);
+  for (let i = 0; i < counterKeys.length; i++) {
+    if (prevKey) counter[counterKeys[i]] += counter[prevKey];
+    prevKey = counterKeys[i];
   }
   let prevVal = 0;
-  for (const key in counter) {
-    const stash = counter[key];
-    counter[key] = prevVal;
+  for (let i = 0; i < counterKeys.length; i++) {
+    const stash = counter[counterKeys[i]];
+    counter[counterKeys[i]] = prevVal;
     prevVal = stash;
   }
   const output = new Array(arr.length);
   for (let i = 0; i < arr.length; i++) {
-    output[counter[arr[i]]] = map[i];
+    output[counter[arr[i]]] = arr[i];
     counter[arr[i]]++;
   }
-  for (let i = 0; i < arr.length; i++) map[i] = output[i];
+  for (let i = 0; i < arr.length; i++) arr[i] = output[i];
 };
 
-const radixSort = arr => {
-  const arrMax = arr.reduce((acc, next) => acc > next ? acc : next, arr[0]);
-  const arrLen = arr.length;
-  let runs = 0;
-  let toReduce = arrMax;
-  while (toReduce) {
-    runs++;
-    toReduce = (toReduce - (toReduce % 10)) / 10;
-  }
-  // the reduced number % 10 ^ remaining runs
-  const disposeMap = new Array(arrLen);
-  for (let i = 0; i < runs; i++) {
-    for (let j = 0; j < arrLen; j++) {
-      const tenPow = Math.pow(10, i);
-      disposeMap[j] = (arr[j] - (arr[j] % tenPow)) / tenPow;
-    }
-    countSort(disposeMap, arr, arrMax);
-  }
-};
-
-console.log(unsorted);
-radixSort(unsorted);
-console.log(unsorted);
-// countSort(unsorted);
+const someArr = new Array(100);
+for (let i = 0; i < 100; i++) someArr[i] = Math.floor(Math.random() * 1000);
+countSort(someArr);
+console.log(someArr);
