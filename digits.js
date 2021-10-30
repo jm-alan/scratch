d759cafd6f47ee9fb3bb841bcf408c9fd697f483
JM <j.maxwell.alan@gmail.com>
2021-10-30 02:03:22 -0400

    create getNthDigitFromEnd helper function

---
+++ digits.js
@@ -0,0 +1,8 @@
+const getNthDigitFromEnd = (num, distanceFromEnd) => {
+  const lastDigit = num % 10;
+  if (!distanceFromEnd) return lastDigit;
+  num = (num - lastDigit) / 10;
+  return getNthDigitFromEnd(num, distanceFromEnd - 1);
+};
+const someNum = 8293759287345;
+console.log(getNthDigitFromEnd(someNum, 0));
