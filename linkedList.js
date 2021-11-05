25e452e4bd2f4292270903b58d1bab66c0022f47
JM <j.maxwell.alan@gmail.com>
2021-11-05 14:47:30 -0400

    upgrades?

--- linkedList.js
+++ linkedList.js
@@ -6,10 +6,10 @@ class Node {
 }
 
 class List {
-  constructor (head, tail) {
+  constructor () {
     this.length = 0;
-    this.head = head;
-    this.tail = tail;
+    this.head = undefined;
+    this.tail = undefined;
   }
 
   pushNode (node) {
@@ -31,6 +31,7 @@ class List {
     this.tail.next = undefined;
     list.length--;
     this.length++;
+    if (list.length === 0) list.tail = undefined;
   }
 
   mergeSort () {
@@ -38,8 +39,10 @@ class List {
     const half = this.length / 2;
     const leftLength = Math.floor(half);
     const left = new List();
+    const right = new List();
     while (left.length < leftLength) left.decapitate(this);
-    const right = new List(this.head, this.tail);
+    right.head = this.head;
+    right.tail = this.tail;
     right.length = Math.ceil(half);
     this.length = 0;
     right.mergeSort();
@@ -49,8 +52,7 @@ class List {
 
   static merge (list1, list2, list3) {
     while (list1.length && list2.length) {
-      if (list1.head.val < list2.head.val) list3.decapitate(list1);
-      else list3.decapitate(list2);
+      (list1.head.val < list2.head.val) ? list3.decapitate(list1) : list3.decapitate(list2);
     }
     if (list1.length) {
       list3.tail.next = list1.head;
@@ -88,8 +90,12 @@ for (let i = 0; i < 30; i++) {
   let timer = global.performance.now();
   list.mergeSort();
   timers.push(timer = global.performance.now() - timer);
-  if (!List.isSorted(list)) throw new Error('SORT FAILED');
   speeds.push(runs / timer);
+  if (!List.isSorted(list)) {
+    console.log(timers, timers.length);
+    console.log(speeds, speeds.length);
+    throw new Error('SORT FAILED');
+  }
 }
 console.log(timers.reduce((acc, next) => acc + next) / 30, speeds.reduce((acc, next) => acc + next) / 30);
 console.log(global.performance.now() - globalTimer);
