c820182505529f7579d47cae586576b351c622ff
JM <j.maxwell.alan@gmail.com>
2021-11-16 18:30:22 -0500

    moar speed

--- listFunctions.js
+++ listFunctions.js
@@ -1,4 +1,6 @@
-const pushNode = (list, node) => {
+const { workerData, parentPort } = require('worker_threads');
+
+global.pushNode = (list, node) => {
   if (list.tail) {
     list.tail.next = node;
     list.tail = list.tail.next;
@@ -9,7 +11,7 @@ const pushNode = (list, node) => {
   list.length++;
 };
 
-const decapitate = (consumptor, list) => {
+global.decapitate = (consumptor, list) => {
   if (consumptor.length) consumptor.tail.next = list.head;
   else consumptor.head = list.head;
   consumptor.tail = list.head;
@@ -20,8 +22,8 @@ const decapitate = (consumptor, list) => {
   if (list.length === 0) list.tail = undefined;
 };
 
-const merge = (list1, list2, list3) => {
-  while (list1.length && list2.length) (list1.head.val < list2.head.val) ? decapitate(list3, list1) : decapitate(list3, list2);
+global.merge = (list1, list2, list3) => {
+  while (list1.length && list2.length) (list1.head.val < list2.head.val) ? global.decapitate(list3, list1) : global.decapitate(list3, list2);
   if (list1.length) {
     list3.tail.next = list1.head;
     list3.tail = list1.tail;
@@ -37,10 +39,10 @@ const merge = (list1, list2, list3) => {
   }
 };
 
-const mergeSort = list => {
+global.mergeSort = list => {
   if (list.length < 2) return;
   const half = list.length / 2;
-  const leftLength = Math.floor(half);
+  const leftLength = global.Math.floor(half);
   const left = {
     length: 0,
     head: undefined,
@@ -51,33 +53,33 @@ const mergeSort = list => {
     head: undefined,
     tail: undefined
   };
-  while (left.length < leftLength) decapitate(left, list);
+  while (left.length < leftLength) global.decapitate(left, list);
   right.head = list.head;
   right.tail = list.tail;
-  right.length = Math.ceil(half);
+  right.length = global.Math.ceil(half);
   list.length = 0;
-  mergeSort(left);
-  mergeSort(right);
-  merge(left, right, list);
+  global.mergeSort(left);
+  global.mergeSort(right);
+  global.merge(left, right, list);
 };
 
 const globalTimer = global.performance.now();
 const timers = [];
 const speeds = [];
-const runs = +process.argv[2];
+const runs = workerData;
 for (let i = 0; i < 100; i++) {
   const list = {
     length: 0,
     head: undefined,
     tail: undefined
   };
-  for (let j = 0; j < runs; j++) pushNode(list, { val: (Math.round(Math.random() * 1000000000)), next: undefined });
+  for (let j = 0; j < runs; j++) global.pushNode(list, { val: (Math.round(Math.random() * 1000000000)), next: undefined });
   let timer = global.performance.now();
-  mergeSort(list);
+  global.mergeSort(list);
   timers.push(timer = global.performance.now() - timer);
   speeds.push(runs / timer);
 }
-console.log({
+parentPort.postMessage({
   'List type': 'POJO',
   'Node type': 'POJO',
   'Function type': 'external',
